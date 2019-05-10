cd48bd5a286b39d6a7d0638116367788510ac186
root <root@ip-172-31-26-154.ap-south-1.compute.internal>
2019-05-10 05:22:24 +0000

    Added logging functionality with log file

--- fl_app.py
+++ fl_app.py
@@ -7,4 +7,9 @@ def home():
     return "Hello, World!"
 
 if __name__ == "__main__":
+
+    import logging
+    logging.basicConfig(filename='debug.log',level=logging.DEBUG)
+    logging.basicConfig(filename='error.log',level=logging.ERROR)
+
     app.run(host='0.0.0.0', port=5000, debug=True)
